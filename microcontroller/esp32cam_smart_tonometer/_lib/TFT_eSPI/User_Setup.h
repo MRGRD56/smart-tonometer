f36083e303823cbc5a8528b94b148c657ffd1430
k.golikov <kgolikov@37bytes.com>
2023-05-01 01:13:32 +0500

    Add TFT_eSPI config

---
+++ microcontroller/esp32cam_smart_tonometer/_lib/TFT_eSPI/User_Setup.h
@@ -0,0 +1,30 @@
+#define USER_SETUP_INFO "User_Setup"
+
+#define ST7735_DRIVER
+
+#define TFT_WIDTH  128
+#define TFT_HEIGHT 160
+
+#define ST7735_BLACKTAB
+
+#define TFT_MISO -1
+#define TFT_MOSI 15
+#define TFT_SCLK 14
+#define TFT_CS   13
+#define TFT_DC    2
+#define TFT_RST  12
+
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define LOAD_FONT6
+#define LOAD_FONT7
+#define LOAD_FONT8
+#define LOAD_GFXFF
+
+#define SMOOTH_FONT
+
+#define SPI_FREQUENCY  27000000
+
+#define SPI_READ_FREQUENCY  20000000
+#define SPI_TOUCH_FREQUENCY  2500000
